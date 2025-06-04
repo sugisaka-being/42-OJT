505db386bb1015d256de0af9fdaa602c2e4c9594
BEING\seki <seki@beingcorp.co.jp>
2025-06-04 09:55:34 +0900

    #559095 プレイヤーの動作の作成
    プレイヤーのPictureBoxがShiftキーを押すことで地面と天井を切り替えて張り付く挙動を実装しました。

--- ShinobiSHift_Game/ShinobiSHift_Game/ShinobiShiftInAction.cs
+++ ShinobiSHift_Game/ShinobiSHift_Game/ShinobiShiftInAction.cs
@@ -12,9 +12,33 @@ namespace ShinobiSHift_Game
 {
     public partial class ShinobiShiftInAction : Form
     {
+        private bool isOnCeiling = false;
+
+        private int playerX = 158;
+        private int groundY = 272;
+        private int ceilingY = 0;
+
         public ShinobiShiftInAction()
         {
             InitializeComponent();
+        }
+
+        private void shift(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.ShiftKey)
+            {
+
+                isOnCeiling = !isOnCeiling;
+
+                if (isOnCeiling)
+                {
+                    Player.Location = new Point(this.playerX, ceilingY);
+                }
+                else
+                {
+                    Player.Location = new Point(this.playerX, groundY);                    
+                }
+            }
         }
     }
 }
