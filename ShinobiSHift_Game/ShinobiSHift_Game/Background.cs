79f17d7d5f8ce0386cd415e42eea95e7d4abc82e
BEING\seki <seki@beingcorp.co.jp>
2025-06-11 13:56:50 +0900

    #560657 背景の作成
    背景画像を作成しプレイ画面で左から右に流れる機能を実装しました

---
+++ ShinobiSHift_Game/ShinobiSHift_Game/Background.cs
@@ -0,0 +1,42 @@
+﻿using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+
+namespace ShinobiSHift_Game
+{
+    public class ParallaxLayer
+    {
+        private Bitmap image;
+        private int x;
+        private int speed;
+
+        public ParallaxLayer(string imagePath, int scrollSpeed)
+        {
+            image = new Bitmap(imagePath);
+            speed = scrollSpeed;
+            x = 0;
+        }
+
+        public void Update()//位置を更新するメソッド
+        {
+            int margin = 500;
+            x -= speed;
+            if (x <= -image.Width - margin)
+            {
+                x += image.Width;
+            }
+        }
+
+        public void Draw(Graphics g, int canvasHeight, int canvasWidth)//描画メソッド
+        {
+            int y = canvasHeight - image.Height;
+
+            for (int i = 0; i < 3; i++) //3枚描画
+            {
+                g.DrawImage(image, x + i * image.Width, y);
+            }
+        }
+        public void Dispose() => image.Dispose();
+    }
+
+}
