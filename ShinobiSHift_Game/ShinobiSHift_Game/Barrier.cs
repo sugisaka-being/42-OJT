b2c18b15834bc4cd3b288ff607d6953e6726f1e8
BEING\seki <seki@beingcorp.co.jp>
2025-06-12 17:23:46 +0900

    #561119 障害物のテクスチャの実装
    障害物用の画像を作成し、ゲームに実装しました。

--- ShinobiSHift_Game/ShinobiSHift_Game/Barrier.cs
+++ ShinobiSHift_Game/ShinobiSHift_Game/Barrier.cs
@@ -1,10 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static ShinobiLeap_Game.PlayForm;
 
 namespace ShinobiLeap_Game
 {
@@ -18,7 +21,9 @@ namespace ShinobiLeap_Game
             {
                 Location = new Point(x, y),
                 Size = new Size(width, height),
-                BackColor = Color.White
+                SizeMode = PictureBoxSizeMode.StretchImage,
+                Image = ImageCache.BarrierImage,
+                BackColor = Color.Transparent
             };
             parent.Controls.Add(PictureBox);
         }
